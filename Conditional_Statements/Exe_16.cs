ae93297a5251068ea6dbf95ef43d3eae6f9f0295
Sayyab Khan <sayyabkhan007@gmail.com>
2023-09-07 15:03:05 -0700

    Solved Exercise 16

---
+++ Conditional_Statements/Exe_16.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conditional_Statements
+{
+    internal class Exe_16
+    {
+        //16. Write a C# Sharp program to check whether an alphabet letter is a vowel or a consonant.
+
+        public static void CheckInput()
+        {
+            Console.WriteLine("Enter an alphabet");
+            char input = char.Parse(Console.ReadLine());
+
+            if(input == 'a' || input == 'e' || input == 'i' || input == 'o' || input == 'u')
+            {
+                Console.WriteLine("The alphabet is a vowel");
+            }
+            else
+            {
+                Console.WriteLine("The alphabet is consonant");
+            }
+        }
+    }
+}
