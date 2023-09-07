958988808d6de831e832169b5cbfa8e4c9d6dff4
Sayyab Khan <sayyabkhan007@gmail.com>
2023-09-07 11:36:10 -0700

    Solved Exercise 1

---
+++ Conditional_Statements/Exe_1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Conditional_Statements
+{
+    internal class Exe_1
+    {
+        //1. Write a C# Sharp program to accept two integers
+        //and check whether they are equal or not.
+
+        public static void CheckInput()
+        {
+            Console.WriteLine("Enter num1");
+            int num1 = int.Parse(Console.ReadLine()); 
+            Console.WriteLine("Enter num2");
+            int num2 = int.Parse(Console.ReadLine());
+
+               
+            if(num1 == num2)
+            {
+                Console.WriteLine($"{num1} and {num2} are equal");
+            }
+            else
+            {
+                Console.WriteLine("Number are different");
+            }
+        }
+        
+    }
+}
